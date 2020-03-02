0bcd16ca863ca80a57ef5271cd1527259eb72d56
Alexandr <alex@MacBook-Pro-Alexandr.local>
2020-03-02 12:39:38 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -70,4 +70,18 @@ $(document).ready(function(){
           });
           return false;
     })
-  });
+
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+
+    window.addEventListener('scroll', function() {
+        if(window.pageYOffset >= 700) {
+            $('.pageUp').fadeIn();
+        } else {
+            $('.pageUp').fadeOut();
+        }
+    });
+});
